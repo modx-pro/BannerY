f20781ab7ab4629bd9688abb9bb96564e93ea1fc
bezumkin <bezumkin@yandex.ru>
2012-04-23 11:27:18 +0400

    Russian lexicon

---
+++ core/components/bannerx/lexicon/ru/default.inc.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Default English Lexicon Entries for BannerX
+ *
+ * @package bannerx
+ * @subpackage lexicon
+ */
+$_lang['bannerx'] = 'BannerX';
+$_lang['bannerx.desc'] = 'Управление рекламой';
+
+$_lang['bannerx.ads'] = 'Баннеры';
+$_lang['bannerx.ads.name'] = 'Название';
+$_lang['bannerx.ads.url'] = 'URL';
+$_lang['bannerx.ads.image'] = 'Изображение';
+$_lang['bannerx.ads.image.new'] = 'Новое изображение';
+$_lang['bannerx.ads.image.current'] = 'Текущее изображение';
+$_lang['bannerx.ads.active'] = 'Активно';
+$_lang['bannerx.ads.clicks'] = 'Клики';
+$_lang['bannerx.ads.new'] = 'Новый баннер';
+$_lang['bannerx.ads.update'] = 'Обновить баннер';
+$_lang['bannerx.ads.remove'] = 'Удалить баннер';
+$_lang['bannerx.ads.remove.confirm'] = 'Вы уверены, что хотите удалить этот баннер?';
+$_lang['bannerx.ads.error.nf'] = 'Баннер не найден';
+
+$_lang['bannerx.positions'] = 'Позиции';
+$_lang['bannerx.positions.name'] = 'Название';
+$_lang['bannerx.positions.clicks'] = 'Клики';
+$_lang['bannerx.positions.new'] = 'Новая позиция';
+$_lang['bannerx.positions.update'] = 'Обновить позицию';
+$_lang['bannerx.positions.remove'] = 'Удалить позицию';
+$_lang['bannerx.positions.remove.confirm'] = 'Вы уверены, что хотите удалить эту позицию?';
+$_lang['bannerx.positions.error.nf'] = 'Позиция не найдена';
+
+$_lang['bannerx.stats'] = 'Статистика';
+$_lang['bannerx.stats.clicks'] = 'Клики';
+$_lang['bannerx.stats.referrers'] = 'Рефереры';
+$_lang['bannerx.stats.referrer'] = 'Реферер';
+$_lang['bannerx.stats.thismonth'] = 'Текущий месяц';
+$_lang['bannerx.stats.lastmonth'] = 'Прошлый месяц';
+$_lang['bannerx.stats.thisyear'] = 'Текущий год';
+$_lang['bannerx.stats.today'] = 'Сегодня';
+$_lang['bannerx.stats.overall'] = 'За все время';
