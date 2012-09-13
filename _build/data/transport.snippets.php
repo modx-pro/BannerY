43d703f9b91c3b8d7c19adbbe35e050d0a802bed
bezumkin <bezumkin@yandex.ru>
2012-09-13 23:53:23 +0700

    Added snippet properties for BannerX

--- _build/data/transport.snippets.php
+++ _build/data/transport.snippets.php
@@ -1,9 +1,12 @@
 <?php
 $snippets = array();
+$properties = include $sources['build'].'properties/properties.bannerx.php';
+
 $snippets[0] = $modx->newObject('modSnippet');
 $snippets[0]->set('id', 0);
 $snippets[0]->set('name', 'BannerX');
 $snippets[0]->set('description', 'Show ads on your site');
 $snippets[0]->set('snippet', file_get_contents($sources['source_core'].'/elements/snippets/bannerx.snippet.php'));
+$snippets[0]->setProperties($properties[0]);
 
 return $snippets;
---
+++ _build/properties/properties.bannerx.php
@@ -0,0 +1,49 @@
+<?php
+$properties[0] = array(
+	array(
+		'name' => 'position',
+		'value' => 0,
+		'type' => 'numberfield',
+		'desc' => 'If set to non-zero, will retrieve only ads that are assigned to the position given.',
+	),
+	array(
+		'name' => 'tpl',
+		'value' => 'bxAd',
+		'type' => 'textfield',
+		'desc' => 'Name of a chunk for templating an Ad.',
+	),
+	array(
+		'name' => 'limit',
+		'value' => 5,
+		'type' => 'numberfield',
+		'desc' => 'If set to non-zero, will only show X number of items.',
+	),
+	array(
+		'name' => 'sortby',
+		'value' => 'RAND()',
+		'type' => 'textfield',
+		'desc' => 'Return results in specified order. It can be any field of bxAd, "RAND()" or "idx" - index of ad in position.',
+        'options' => array(
+            array('text' => 'Random','value' => 'RAND()'),
+            array('text' => 'Index','value' => 'idx'),
+            array('text' => 'Name','value' => 'name'),
+            array('text' => 'Url','value' => 'url'),
+            array('text' => 'Image','value' => 'image'),
+            array('text' => 'Active','value' => 'active'),
+            array('text' => 'Description','value' => 'description'),
+        ),
+	),
+	array(
+		'name' => 'sortdir',
+		'value' => 'ASC',
+		'type' => 'list',
+		'desc' => 'Order of the results',
+        'options' => array(
+            array('text' => 'ASC','value' => 'ASC'),
+            array('text' => 'DESC','value' => 'DESC'),
+        ),
+	),
+);
+
+return $properties;
+?>
