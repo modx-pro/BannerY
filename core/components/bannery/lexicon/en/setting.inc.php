34bb1611eafaa36423e72f1d4b49ac7882c3c93a
Bezumkin <bezumkin@yandex.ru>
2014-07-31 10:18:07 +0700

    Fixed lexicon entry

--- core/components/bannery/lexicon/en/setting.inc.php
+++ core/components/bannery/lexicon/en/setting.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$_lang['area_modextra_main'] = 'Main';
+$_lang['area_bannery_main'] = 'Main';
 
 $_lang['setting_bannery_click'] = 'Banner click variable';
 $_lang['setting_bannery_click_desc'] = 'Specify the variable on which you need to consider that there was a click on the banner.';
