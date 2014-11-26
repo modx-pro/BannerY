b74c48fb3536f61188d8a7d72f375d2b066afb70
Romain <romain@melting-media.com>
2014-11-26 09:23:48 +0100

    French translation for the CMP

---
+++ core/components/bannery/lexicon/fr/default.inc.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Default French Lexicon Entries for BannerY
+ *
+ * @package bannery
+ * @subpackage lexicon
+ */
+$_lang['bannery'] = 'BannerY';
+$_lang['bannery.desc'] = 'BannerY - gérez vos bannières';
+
+$_lang['bannery.ads'] = 'Bannières';
+$_lang['bannery.ads.name'] = 'Nom';
+$_lang['bannery.ads.url'] = 'URL';
+$_lang['bannery.ads.url.description'] = 'Vous pouvez indiquer une ressource ou une adresse HTTP';
+$_lang['bannery.ads.image'] = 'Image';
+$_lang['bannery.ads.image.new'] = 'Nouvelle image';
+$_lang['bannery.ads.image.current'] = 'Image actuelle';
+$_lang['bannery.ads.active'] = 'Active';
+$_lang['bannery.ads.clicks'] = 'Clics';
+$_lang['bannery.ads.new'] = 'Nouvelle bannière';
+$_lang['bannery.ads.update'] = 'Mettre à jour';
+$_lang['bannery.ads.remove'] = 'Supprimer';
+$_lang['bannery.ads.remove.confirm'] = 'Êtes-vous sûr de vouloir supprimer cette bannière ?';
+$_lang['bannery.ads.error.nf'] = 'Bannière non trouvée';
+$_lang['bannery.ads.description'] = 'Texte de la bannière';
+$_lang['bannery.ads.enable'] = 'Activer';
+$_lang['bannery.ads.disable'] = 'Désactiver';
+$_lang['bannery.ads.add'] = 'Ajouter';
+$_lang['bannery.ads.error.ns'] = 'Bannière non définie';
+$_lang['bannery.ads.source'] = 'Media source';
+$_lang['bannery.ads.start'] = 'Début d\'affichage';
+$_lang['bannery.ads.end'] = 'Fin d\'affichage';
+
+$_lang['bannery.positions'] = 'Emplacements';
+$_lang['bannery.positions.name'] = 'Nom';
+$_lang['bannery.positions.clicks'] = 'Clics';
+$_lang['bannery.positions.new'] = 'Nouvel emplacement';
+$_lang['bannery.positions.update'] = 'Mettre à jour';
+$_lang['bannery.positions.remove'] = 'Supprimer';
+$_lang['bannery.positions.select'] = 'Sélectionner';
+$_lang['bannery.positions.remove.confirm'] = 'Êtes-vous sûr de vouloir supprimer cet emplacement ?';
+$_lang['bannery.positions.error.nf'] = 'Emplacement non trouvé';
+$_lang['bannery.positions.error.ns'] = 'Emplacement non défini';
+
+$_lang['bannery.adposition.idx'] = 'Index';
+$_lang['bannery.adposition.remove'] = 'Supprimer';
+$_lang['bannery.adposition.error.ae'] = 'Cette bannière est déjà active pour cet emplacement.';
+
+$_lang['bannery.stats'] = 'Statistiques';
+$_lang['bannery.stats.clicks'] = 'Clics';
+$_lang['bannery.stats.referrers'] = 'Referrers';
+$_lang['bannery.stats.referrer'] = 'Referrer';
+$_lang['bannery.stats.thismonth'] = 'Ce mois';
+$_lang['bannery.stats.lastmonth'] = 'Mois passé';
+$_lang['bannery.stats.thisyear'] = 'Cette année';
+$_lang['bannery.stats.today'] = 'Aujourd\'hui';
+$_lang['bannery.stats.overall'] = 'Depuis le début';
+
+$_lang['bannery.error.no_positions'] = 'Vous devez créer au moins un emplacement pour y ajouter des bannières.';
